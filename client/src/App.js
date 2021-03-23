73b2fb43093e6cc4a5233c73003e5adcded89a62
Thomas Capello <thomcapello@gmail.com>
2021-03-22 20:31:05 -0400

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route  } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+  // const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
@@ -12,8 +14,10 @@ export default function App () {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
+          console.log(response)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -29,8 +33,14 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <div>
+          <Route path="/">
+            <MovieList movies={movieList} />
+          </Route>
+          <Route path={'/movies/:id'}>
+            <Movie />
+          </Route>
+      </div>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom'
 
 export default function MovieList(props) {
+
+  const { movies } = props
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+            <MovieDetails movie={movie} to={`/movies/${movie.id}`}/>
       ))}
     </div>
   );
@@ -13,8 +17,13 @@ export default function MovieList(props) {
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
+  const history = useHistory()
+  const routeToDets = () => {
+    history.push(`/movies/${props.movie.id}`)
+  }
+
   return (
-    <div className="movie-card">
+    <div onClick={routeToDets}className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
